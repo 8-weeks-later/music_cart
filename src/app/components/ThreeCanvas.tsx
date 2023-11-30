9e8c32e576ca8bef9ea54d83e25449def6d6b066
dev-hamster <hamsterwave0925@gmail.com>
2023-11-30 23:41:45 +0900

    feat: 카메라가 하단을 보도록 위치

---
+++ src/app/components/ThreeCanvas.tsx
@@ -0,0 +1,32 @@
+import { Canvas, useThree } from "@react-three/fiber";
+
+export default function ThreeCanvas({ children }: any) {
+  return (
+    <Canvas
+      dpr={[1, 2]}
+      gl={{ alpha: false }}
+      camera={{
+        position: [0, 20, 0],
+        fov: 45,
+      }}
+    >
+      <color attach="background" args={["lightblue"]} />
+      <ambientLight />
+      <directionalLight
+        position={[10, 10, 10]}
+        castShadow
+        shadow-mapSize={[2048, 2048]}
+      />
+      <directionalLight intensity={1} />
+      <ambientLight intensity={1.2} />
+      <spotLight
+        intensity={0.1}
+        angle={0.1}
+        penumbra={1}
+        position={[10, 15, 10]}
+        castShadow
+      />
+      {children}
+    </Canvas>
+  );
+}
