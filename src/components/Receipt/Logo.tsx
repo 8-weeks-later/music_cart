c2ca22dd523dad37ccf10a407dd7c020dea5f177
dev-hamster <hamsterwave0925@gmail.com>
2023-12-09 02:41:58 +0900

    design: 영수증 디자인

---
+++ src/components/Receipt/Logo.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import styled from "@emotion/styled";
+
+export default function Logo() {
+  return (
+    <Container>
+      <LogoText>
+        <span>뮤직마켓</span>
+        <Image
+          alt="logo"
+          src="/assets/images/logo-black.png"
+          width={55}
+          height={46}
+        />
+      </LogoText>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  margin: 70px 0 32px;
+`;
+
+const LogoText = styled.div`
+  display: flex;
+  justify-content: center;
+
+  color: #000;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 700;
+`;
