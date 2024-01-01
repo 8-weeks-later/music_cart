6909cecddd4ab5d5de0625d00739bddaffd215f6
dev-hamster <hamsterwave0925@gmail.com>
2024-01-01 14:53:33 +0900

    feat: spotify verifier 발급

---
+++ src/utils/spotify.ts
@@ -0,0 +1,40 @@
+import { SPOTIFY_CLIENT_ID, SPOTIFY_REDIRECT_URI } from "@/constants";
+
+export async function redirectToAuthCodeFlow() {
+  const verifier = generateCodeVerifier(128);
+  const challenge = await generateCodeChallenge(verifier);
+
+  localStorage.setItem("verifier", verifier);
+
+  const params = new URLSearchParams();
+  params.append("client_id", SPOTIFY_CLIENT_ID);
+  params.append("response_type", "code");
+  params.append("redirect_uri", SPOTIFY_REDIRECT_URI);
+  params.append("scope", "user-read-recently-played");
+  params.append("code_challenge_method", "S256");
+  params.append("code_challenge", challenge);
+
+  return `https://accounts.spotify.com/authorize?${params.toString()}`;
+}
+
+function generateCodeVerifier(length: number) {
+  let text = "";
+  let possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+export async function generateCodeChallenge(codeVerifier: string) {
+  const data = new TextEncoder().encode(codeVerifier);
+  const digest = await window.crypto.subtle.digest("SHA-256", data);
+
+  // @ts-ignore
+  return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=+$/, "");
+}
